de9ef603303d33c9e10065158d13b212d77e71d5
BrechREiZ <baphomet@herr-der-mails.de>
2019-03-28 21:38:37 +0100

    add dependencies and change electrum data directory

--- electrum/util.py
+++ electrum/util.py
@@ -483,11 +483,11 @@ def user_dir():
     if 'ANDROID_DATA' in os.environ:
         return android_data_dir()
     elif os.name == 'posix':
-        return os.path.join(os.environ["HOME"], ".electrum")
+        return os.path.join(os.environ["HOME"], ".electrum-ecc")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Electrum-ECC")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum-ECC")
     else:
         #raise Exception("No home directory found in environment variables.")
         return
